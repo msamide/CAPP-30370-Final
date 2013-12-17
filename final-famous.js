b191df8a9f96e754a0657932517b634eb722d866
msamide <msamide@nd.edu>
2013-12-16 23:08:29 -0500

    Final Commit

---
+++ final-famous.js
@@ -0,0 +1,263 @@
+$(window).on('load resize', function(){
+  var $widthvalue = $(window).width();
+  console.log('width', $widthvalue);
+});
+
+
+$('#buttonobama').on('click', function(){
+  var image = $('#obamapic');
+  image.attr('src', "http://assets.inhabitat.com/wp-content/blogs.dir/1/files/2013/01/president-barack-obama-2013-inauguration-537x442.jpg");
+  image.attr('alt', 'New Obama Pic');
+});
+
+
+
+$('#ariespic').on("click", function(){
+  var ariespic = $('#ariespic');
+  var width = ariespic.width();
+  var height = ariespic.height();
+  console.log('Title:', ariespic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('#liupic').on("click", function(){
+  var liupic = $('#liupic');
+  var width = liupic.width();
+  var height = liupic.height();
+  console.log('Title:', liupic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('#renaldopic').on("click", function(){
+  var renaldopic = $('#renaldopic');
+  var width = renaldopic.width();
+  var height = renaldopic.height();
+  console.log('Title:', renaldopic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('#obamapic').on("click", function(){
+  var obamapic = $('#obamapic');
+  var width = obamapic.width();
+  var height = obamapic.height();
+  console.log('Title:', obamapic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('#mosespic').on("click", function(){
+  var mosespic = $('#mosespic');
+  var width = mosespic.width();
+  var height = mosespic.height();
+  console.log('Title:', mosespic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('#felixpic').on("click", function(){
+  var felixpic = $('#felixpic');
+  var width = felixpic.width();
+  var height = felixpic.height();
+  console.log('Title:', felixpic.attr('alt'));
+  console.log('width', width);
+  console.log('height', height);
+});
+
+
+$('.header11').on('click', function(){
+  var header11 = $('.header11');
+  console.log('Text:', $(this).text());
+  var fontsize = header11.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header11.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header12').on('click', function(){
+  var header12 = $('.header12');
+  console.log('Text:', $(this).text());
+  var fontsize = header12.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header12.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header13').on('click', function(){
+  var header13 = $('.header13');
+  console.log('Text:', $(this).text());
+  var fontsize = header13.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header13.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header14').on('click', function(){
+  var header14 = $('.header14');
+  console.log('Text:', $(this).text());
+  var fontsize = header14.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header14.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header15').on('click', function(){
+  var header15 = $('.header15');
+  console.log('Text:', $(this).text());
+  var fontsize = header15.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header15.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header16').on('click', function(){
+  var header16 = $('.header16');
+  console.log('Text:', $(this).text());
+  var fontsize = header16.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header16.css('color');
+  console.log('Text Color:', color);
+});
+
+$('.header17').on('click', function(){
+  var header17 = $('.header17');
+  console.log('Text:', $(this).text());
+  var fontsize = header17.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header17.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header12').on('click', function(){
+  var header12 = $('#header12');
+  console.log('Text:', $(this).text());
+  var fontsize = header12.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header12.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header13').on('click', function(){
+  var header13 = $('#header13');
+  console.log('Text:', $(this).text());
+  var fontsize = header13.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header13.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header14').on('click', function(){
+  var header14 = $('#header14');
+  console.log('Text:', $(this).text());
+  var fontsize = header14.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header14.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header15').on('click', function(){
+  var header15 = $('#header15');
+  console.log('Text:', $(this).text());
+  var fontsize = header15.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header15.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header16').on('click', function(){
+  var header16 = $('#header16');
+  console.log('Text:', $(this).text());
+  var fontsize = header16.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header16.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header17').on('click', function(){
+  var header17 = $('#header17');
+  console.log('Text:', $(this).text());
+  var fontsize = header17.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header17.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header18').on('click', function(){
+  var header18 = $('#header18');
+  console.log('Text:', $(this).text());
+  var fontsize = header18.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header18.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header19').on('click', function(){
+  var header19 = $('#header19');
+  console.log('Text:', $(this).text());
+  var fontsize = header19.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header19.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header20').on('click', function(){
+  var header20 = $('#header20');
+  console.log('Text:', $(this).text());
+  var fontsize = header20.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header20.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header21').on('click', function(){
+  var header21 = $('#header21');
+  console.log('Text:', $(this).text());
+  var fontsize = header21.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header21.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header22').on('click', function(){
+  var header22 = $('#header22');
+  console.log('Text:', $(this).text());
+  var fontsize = header22.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header22.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header23').on('click', function(){
+  var header23 = $('#header23');
+  console.log('Text:', $(this).text());
+  var fontsize = header23.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header23.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header24').on('click', function(){
+  var header24 = $('#header24');
+  console.log('Text:', $(this).text());
+  var fontsize = header24.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header24.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header25').on('click', function(){
+  var header25 = $('#header25');
+  console.log('Text:', $(this).text());
+  var fontsize = header25.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header25.css('color');
+  console.log('Text Color:', color);
+});
+
