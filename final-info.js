181f9f530819d48aa79038dd31826a10b803d382
msamide <msamide@nd.edu>
2013-12-16 23:03:25 -0500

    Final Commit

---
+++ final-info.js
@@ -0,0 +1,68 @@
+$(window).on('load resize', function(){
+  var $widthvalue = $(window).width();
+  console.log('width', $widthvalue);
+});
+
+$('.header4').on('click', function(){
+  var header4 = $('.header4');
+  console.log('Text:', $(this).text());
+  var fontsize = header4.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header4.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header5').on('click', function(){
+  var header5 = $('#header5');
+  console.log('Text:', $(this).text());
+  var fontsize = header5.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header5.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header6').on('click', function(){
+  var header6 = $('#header6');
+  console.log('Text:', $(this).text());
+  var fontsize = header6.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header6.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header7').on('click', function(){
+  var header7 = $('#header7');
+  console.log('Text:', $(this).text());
+  var fontsize = header7.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header7.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header8').on('click', function(){
+  var header8 = $('#header8');
+  console.log('Text:', $(this).text());
+  var fontsize = header8.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header8.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header9').on('click', function(){
+  var header9 = $('#header9');
+  console.log('Text:', $(this).text());
+  var fontsize = header9.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header9.css('color');
+  console.log('Text Color:', color);
+});
+
+$('#header10').on('click', function(){
+  var header10 = $('#header10');
+  console.log('Text:', $(this).text());
+  var fontsize = header10.css('font-size');
+  console.log('Font Size:', fontsize);
+  var color = header10.css('color');
+  console.log('Text Color:', color);
+});
+
